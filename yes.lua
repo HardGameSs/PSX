bec1b5738a58323f957ce135096e06accc6a6685
Hard Games <110551264+HardGameSs@users.noreply.github.com>
2023-02-04 14:29:36 +0300

    Update yes.lua

--- yes.lua
+++ yes.lua
@@ -1,15 +1,13 @@
 local Bank = getsenv(game.Players.LocalPlayer.PlayerScripts.Scripts.GUIs:FindFirstChild("Bank Client"))
 local Inventory = getsenv(game.Players.LocalPlayer.PlayerScripts.Scripts.GUIs.Inventory)
 
-local Hello = {
-	["Remotes"] = {
-		["get my banks"] = {Bank.GetMyBanks, 3},
-		["get bank"] = {Bank.GetBank, 3},
-		["bank deposit"] = {Bank.Deposit, 16},
-		["invite to bank"] = {Bank.InviteToBank, 18},
-		["kick from bank"] = {Bank.KickMember, 9},
-		["lock pet"] = {Inventory.LockPets, 5},
-	}
+return {
+    ["Remotes"] = {
+        ["get my banks"] = {Bank.GetMyBanks, 3},
+        ["get bank"] = {Bank.GetBank, 3},
+        ["bank deposit"] = {Bank.Deposit, 16},
+        ["invite to bank"] = {Bank.InviteToBank, 18},
+        ["kick from bank"] = {Bank.KickMember, 9},
+        ["lock pet"] = {Inventory.LockPets, 5},
+    }
 }
-
-return Hello
